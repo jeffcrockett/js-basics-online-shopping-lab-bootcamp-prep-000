a178f5fec6209ea960802f977d8faef5d2815bed
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-06-23 19:33:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,28 @@ function addToCart(name){
   return `${name} has been added to your cart.`
 }
 
+function viewCart(){
+  var theString = 'In your cart, you have ';
+  if(cart.length == 1){
+    return `${theString} ${cart[0].itemName} at $${cart[0].itemPrice}.` 
+  }
+  
+  else if(cart.length == 2){
+    return `${theString} ${cart[0].itemName} at $${cart[0].itemPrice} and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+  }
+  
+  else {
+    for(var i=0; i<cart.length; i++){
+      if(i===cart.length-1){
+        theString += `and ${cart[i].itemName} at $${cart[i].itemPrice}.`
+        }
+      else{
+        theString += `${cart[i].itemName} at $${cart[i].itemPrice}, `
+        }
+      }
+    }
+  }
+}
 
 
 
